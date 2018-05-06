d21aa60cbc516eed442e68481009e5c51f336f6b
Max Russek <maxwell.russek@gmail.com>
2018-05-05 18:25:27 -0600

    Fix player repository non-transactional problem.

--- app/src/main/java/de/gutenko/roguelike/habittracker/data/habits/FirebaseHabitCompletionRepository.kt
+++ app/src/main/java/de/gutenko/roguelike/habittracker/data/habits/FirebaseHabitCompletionRepository.kt
@@ -1,50 +1,33 @@
 package de.gutenko.roguelike.habittracker.data.habits
 
-import com.androidhuman.rxfirebase2.database.data
 import com.androidhuman.rxfirebase2.database.dataChanges
 import com.androidhuman.rxfirebase2.database.rxRemoveValue
 import com.androidhuman.rxfirebase2.database.rxSetValue
+import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseReference
-import de.gutenko.roguelike.habittracker.data.player.toPlayer
-import de.gutenko.roguelike.habittracker.data.player.toPlayerUpdate
-import de.gutenko.roguelike.habittracker.data.player.unUpdatePlayer
-import de.gutenko.roguelike.habittracker.data.player.updatePlayer
 import io.reactivex.Completable
 import io.reactivex.Observable
 import org.joda.time.LocalDate
 
-class FirebaseHabitCompletionRepository(
-    private val databaseReference: DatabaseReference
-) : HabitCompletionRepository {
-    private val users = databaseReference.child("users")
-
+class FirebaseHabitCompletionRepository(private val users: DatabaseReference) :
+    HabitCompletionRepository {
     override fun observeHabitCompletion(
         userId: String,
         habitId: String,
         localDate: LocalDate
-    ): Observable<Optional<HabitCompletion>> {
-        return users
+    ): Observable<Optional<HabitCompletion>> =
+        users
             .child(userId)
             .child("completions")
             .child(habitId)
             .child(localDate.toString())
             .dataChanges()
             .map {
-                if (it.exists()) {
-                    val day = LocalDate.parse(it.valueExpected<String>("day"))
-
-                    Optional.Some(
-                        HabitCompletion(
-                            it.valueExpected("userId"),
-                            it.valueExpected("habitId"),
-                            day
-                        )
-                    )
-                } else {
-                    Optional.None
+                when {
+                    it.exists() -> Optional.Some(it.toCompletion())
+                    else -> Optional.None
                 }
             }
-    }
 
     override fun addCompletion(userId: String, habitId: String, localDate: LocalDate): Completable {
         val user = users.child(userId)
@@ -59,38 +42,20 @@ class FirebaseHabitCompletionRepository(
                     "habitId" to habitId,
                     "day" to localDate.toString()
                 )
-            ).andThen(
-                user.child("habits").child(habitId).child("playerUpdate").data()
-                    .map { it.toPlayerUpdate() }
-                    .flatMapCompletable { update ->
-                        val playerRef = user.child("player")
-                        playerRef
-                            .data()
-                            .map { it.toPlayer() }
-                            .flatMapCompletable { player ->
-                                val newPlayer = updatePlayer(player, update)
-
-                                playerRef.rxSetValue(newPlayer)
-                            }
-                    })
+            )
     }
 
     override fun observeCompletionsForPlayer(
         userId: String,
         habitId: String
-    ): Observable<List<HabitCompletion>> {
-        return users
+    ): Observable<List<HabitCompletion>> =
+        users
             .child(userId)
             .child(habitId)
             .dataChanges()
             .map {
-                it.children.map {
-                    val day = LocalDate.parse(it.valueExpected<String>("day"))
-
-                    HabitCompletion(it.valueExpected("userId"), it.valueExpected("habitId"), day)
-                }
+                it.children.map { it.toCompletion() }
             }
-    }
 
     override fun removeCompletion(
         userId: String,
@@ -99,24 +64,18 @@ class FirebaseHabitCompletionRepository(
     ): Completable {
         val user = users.child(userId)
         val completions = user.child("completions")
-        val player = user.child("player")
-        val habits = user.child("habits")
 
         return completions
             .child(habitId)
             .child(localDate.toString())
             .rxRemoveValue()
-            .andThen(
-                habits.child(habitId)
-                    .child("playerUpdate")
-                    .data()
-                    .map { it.toPlayerUpdate() }
-                    .flatMapCompletable { playerUpdate ->
-                        player.data().map { it.toPlayer() }
-                            .flatMapCompletable {
-                                player.rxSetValue(unUpdatePlayer(it, playerUpdate))
-                            }
-                    }
-            )
+    }
+
+    private fun DataSnapshot.toCompletion(): HabitCompletion {
+        return HabitCompletion(
+            valueExpected("userId"),
+            valueExpected("habitId"),
+            LocalDate.parse(valueExpected("day"))
+        )
     }
 }
--- app/src/main/java/de/gutenko/roguelike/habittracker/data/player/Player.kt
+++ app/src/main/java/de/gutenko/roguelike/habittracker/data/player/Player.kt
@@ -6,20 +6,12 @@ import java.io.Serializable
 
 data class Player(
     val userId: String,
-    val userName: String,
-    val attack: Int,
-    val agility: Int,
-    val endurance: Int,
-    val intelligence: Int
+    val userName: String
 ) : Serializable
 
 fun DataSnapshot.toPlayer(): Player = Player(
     valueExpected("userId"),
-    valueExpected("userName"),
-    valueExpected("attack"),
-    valueExpected("agility"),
-    valueExpected("endurance"),
-    valueExpected("intelligence")
+    valueExpected("userName")
 )
 
 
@@ -37,31 +29,4 @@ fun DataSnapshot.toPlayerUpdate(): PlayerUpdate {
         valueExpected("enduranceUpdate"),
         valueExpected("intelligenceUpdate")
     )
-}
-
-
-fun updatePlayer(
-    player: Player,
-    update: PlayerUpdate
-): Player {
-    val newPlayer = player.copy(
-        attack = player.attack + update.attackUpdate,
-        agility = player.agility + update.agilityUpdate,
-        endurance = player.endurance + update.enduranceUpdate,
-        intelligence = player.intelligence + update.intelligenceUpdate
-    )
-    return newPlayer
-}
-
-fun unUpdatePlayer(
-    player: Player,
-    update: PlayerUpdate
-): Player {
-    val newPlayer = player.copy(
-        attack = player.attack - update.attackUpdate,
-        agility = player.agility - update.agilityUpdate,
-        endurance = player.endurance - update.enduranceUpdate,
-        intelligence = player.intelligence - update.intelligenceUpdate
-    )
-    return newPlayer
 }
